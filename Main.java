e9114dec04cde88facebb0cdb7f403cfcbd25f27
AxelGarciaTello <gata21022000@hotmail.com>
2019-12-09 17:36:03 -0600

    Inicio del programa

---
+++ Main.java
@@ -0,0 +1,21 @@
+/*
+Instituto Politécnico Nacional
+Escuela Superior de Cómputo
+Teoria Computacional
+Grupo: 2CV1
+Alumno: García Tello Axel
+Profesor: Benjamin Luna Benoso
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+Lector de MT
+
+Fecha: 24 de noviembre de 2019
+*/
+
+public class Main {
+    
+    public static void main(String[] args){
+        MT inicio=new MT();
+        inicio.iniciarMT();
+    }
+    
+}
